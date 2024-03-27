d56cfdfb98a786800fc580161ea2dfeea5bc0d64
vishnu <vishnu.manubolu@gmail.com>
2024-03-27 16:48:48 +0530

    list empty

---
+++ list.py
@@ -0,0 +1,15 @@
+def empty_list(a):
+
+    if len(a) == 0:
+        print("Empty list")
+    else:
+        print("list is not empty")
+        return a
+ls1 = []
+ls2 = [1,2,3,4]
+print(empty_list(ls1))
+print(empty_list(ls2))
+
+
+
+
